02880e39c05f5e08d81bab061aa618d17144046c
TeppuSan <StarVampire.teppu@gmail.com>
2023-06-23 02:51:03 +0900

    なんかいろいろついか

---
+++ src/TodoList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Todo from './Todo'
+
+
+//↓　コンポーネント
+const TodoList = ({todos}) => {
+  return (
+    //↓　propsのtodosを表示 {}を付けることによってJSの変数を使える
+    //    <div>{props.todos}</div>
+    //<div>{todos}</div>
+    //map関数は配列の要素を一つずつ取り出して処理を行う関数
+    //keyは一意の値を入れる,keyは
+    todos.map((todo) => <Todo todo={todo} key={todo}/>)
+  )
+}
+
+export default TodoList
